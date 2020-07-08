8418a7187e05772dec51eb464da4c6c93ed71c67
Matteo <matteo.senese@gmail.com>
2020-07-08 10:58:58 +0200

    Added data format explanation .txt

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     """Example
 
         python main.py \
-        --data ../simmc/data/simmc_fashion/fashion_train_dials.json
+        --data ../simmc/data/simmc_fashion/fashion_train_dials.json \
+        --metadata ../simmc/data/simmc_fashion/fashion_metadata.json
     """
     parser = argparse.ArgumentParser()
 
@@ -25,16 +26,22 @@ if __name__ == '__main__':
         default=None,
         type=str,
         required=True,
-        help="Path to dataset")
+        help="Path to dataset json file")
+    parser.add_argument(
+        "--metadata",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to metadata json file")
 
     args = parser.parse_args()
-    dataset = SIMMCDatasetForActionPrediction(args.data)
+    dataset = SIMMCDatasetForActionPrediction(args.data, args.metadata)
     print(str(dataset))
 
     loader = DataLoader(dataset)
 
-    #TODO meaning of belief state with length > 1 ??
-    #TODO we always have only one act in the belief state (in the paper they said that multiple actions are possible sometimes)
+    #TODO meaning of belief states with length > 1 ??
+    # maybe it is used to simulate multiple actions. In the paper they said that multiple actions are possible sometimes.
 
     slots = set()
     actions = set()
@@ -49,13 +56,15 @@ if __name__ == '__main__':
             #    for slot in state['slots']:
             #        slots.add(slot[0])
 
+            pdb.set_trace()
+
             #TODO find the API hierarchy
-            for state in dial['belief_state']:
-                if len(state['act']) > 1:
-                    pdb.set_trace()
-                if state['act'][0][:3] == 'ERR':
-                    pdb.set_trace()
-                actions.add(state['act'][0])
+            #for state in dial['belief_state']:
+            #    if len(state['act']) > 1:
+            #        pdb.set_trace()
+            #    if state['act'][0][:3] == 'ERR':
+            #        pdb.set_trace()
+            #    actions.add(state['act'][0])
 
     print(slots)
     pdb.set_trace()
