decoder completed and working. Need to fix the loss computation.

mm_response_generation/models/decoder.py
@@ -12,6 +12,7 @@ class Decoder(nn.Module):
     def __init__(self,
                 d_model,
                 d_enc,
+                d_context,
                 d_k,
                 d_v,
                 d_f,
@@ -33,6 +34,7 @@ class Decoder(nn.Module):
         self.decoder_layers = nn.ModuleList([
                     MultiAttentiveDecoder(d_model=d_model,
                                         d_enc=d_enc,
+                                        d_context=d_context,
                                         d_k=d_k,
                                         d_v=d_v,
                                         d_f=d_f,
@@ -48,13 +50,14 @@ class Decoder(nn.Module):
                                         nn.Linear(d_model//4, self.vocab_size))
 
 
-    def forward(self, input_batch, encoder_out, context, input_mask, enc_mask):
+    def forward(self, input_batch, encoder_out, history_context, visual_context, input_mask, enc_mask):
         assert input_batch.dim() == 2, 'Expected tensor with 2 dimensions but got {}'.format(input_batch)
         assert encoder_out.dim() == 3, 'Expected tensor with 2 dimensions but got {}'.format(encoder_out)
         assert input_mask.dim() == 2, 'Expected tensor with 2 dimensions but got {}'.format(input_mask)
         assert enc_mask.dim() == 2, 'Expected tensor with 2 dimensions but got {}'.format(enc_mask)
         assert input_batch.shape[0] == encoder_out.shape[0], 'Inconsistent batch size'
-        assert input_batch.shape[0] == context.shape[0], 'Inconsistent batch size'
+        assert input_batch.shape[0] == history_context.shape[0], 'Inconsistent batch size'
+        assert input_batch.shape[0] == visual_context.shape[0], 'Inconsistent batch size'
         assert input_batch.shape[0] == input_mask.shape[0], 'Inconsistent batch size'
         assert input_batch.shape[0] == enc_mask.shape[0], 'Inconsistent batch size'
         #input mask is the padding mask
@@ -69,22 +72,22 @@ class Decoder(nn.Module):
         #encoder attention mask avoid 2 things:
         # the decoder to attend to encoder padding (to apply row wise)
         # to use the decoder padding as query (to apply column wise)
-        #pdb.set_trace()
         enc_attn_mask = torch.zeros((input_mask.shape[0], input_mask.shape[1], enc_mask.shape[1])).to(device)
         enc_attn_mask[:, :] = enc_mask[:, None, :]
         enc_attn_mask.transpose_(1, 2)
         enc_attn_mask[:, :] *= input_mask[:, None, :]
         enc_attn_mask.transpose_(1, 2)
 
-        input_embs = self.embedding_layer(input_batch)
-        #pdb.set_trace()
-        x = input_embs
+        x = self.embedding_layer(input_batch)
         for idx in range(len(self.decoder_layers)):
             x = self.decoder_layers[idx](input_embs=x,  
                                         enc_out=encoder_out,
+                                        history_context=history_context,
+                                        visual_context=visual_context,
                                         self_attn_mask=self_attn_mask,
                                         enc_attn_mask=enc_attn_mask)
-        pdb.set_trace()
+        vocab_logits = self.out_layer(x)
+        return vocab_logits
 
 
         """
@@ -158,7 +161,7 @@ class TransformerEmbedding(nn.Module):
 
 class MultiAttentiveDecoder(nn.Module):
 
-    def __init__(self, d_model, d_enc, d_k, d_v, d_f, n_heads, dropout_prob):
+    def __init__(self, d_model, d_enc, d_context, d_k, d_v, d_f, n_heads, dropout_prob):
         super(MultiAttentiveDecoder, self).__init__()
 
         #multi head self attention
@@ -166,35 +169,76 @@ class MultiAttentiveDecoder(nn.Module):
         #fusion layer
         self.multi_head_self = MultiHeadSelfAttention(d_model=d_model, d_k=d_k, d_v=d_v, n_heads=n_heads, dropout_prob=dropout_prob)
         self.multi_head_enc = MultiHeadEncoderAttention(d_model=d_model, d_enc=d_enc, d_k=d_k, d_v=d_v, n_heads=n_heads, dropout_prob=dropout_prob)
-        self.fusion_module = nn.Sequential()
+        self.fusion_module = FusionModule(d_model=d_model, d_context=d_context, dropout_prob=dropout_prob)
 
         self.layerNorm = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(p=dropout_prob)
 
         self.fnn = nn.Sequential(nn.Linear(in_features=d_model, out_features=d_f),
                                 nn.ReLU(),
+                                nn.Dropout(p=dropout_prob),
                                 nn.Linear(in_features=d_f, out_features=d_model))
 
 
 
-    def forward(self, input_embs, enc_out, self_attn_mask, enc_attn_mask):
+    def forward(self, input_embs, enc_out, history_context, visual_context, self_attn_mask, enc_attn_mask):
 
-        self_out = self.multi_head_self(input_embs, self_attn_mask)
+        self_attn_out = self.multi_head_self(input_embs, self_attn_mask)
+        sub_out1 = self.layerNorm(input_embs + self.dropout(self_attn_out))
 
-        #flow:
-        # multi_head_self
-        # add, dropout and norm
-        # multi_head_enc
-        # add, dropout and norm
-        # fusion
-        # add, dropout and norm
-        # fnn
-        # add, dropout and norm
-        pass
+        enc_attn_out = self.multi_head_enc(sub_out1, enc_out, enc_attn_mask)
+        sub_out2 = self.layerNorm(sub_out1 + self.dropout(enc_attn_out))
+
+        fusion_out = self.fusion_module(sub_out2, history_context, visual_context)
+        sub_out3 = self.layerNorm(sub_out2 + self.dropout(fusion_out))
+
+        fnn_out = self.fnn(sub_out3)
+        sub_out4 = self.layerNorm(sub_out3 + self.dropout(fnn_out))
+        
+        return sub_out4
 
 
     def __str__(self):
         return super().__str__()
+
+
+
+class FusionModule(nn.Module):
+    def __init__(self, d_model, d_context, dropout_prob):
+        super(FusionModule, self).__init__()
+
+        self.d_context = d_context
+        self.d_model = d_model
+        d_cat = d_model+d_context
+        self.h_stream = nn.Sequential(nn.Linear(in_features=d_cat, out_features=d_cat//2),
+                                    nn.Linear(in_features=d_cat//2, out_features=d_cat//4),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=dropout_prob),
+                                    nn.Linear(in_features=d_cat//4, out_features=d_cat//2),
+                                    nn.Linear(in_features=d_cat//2, out_features=d_cat))
+        self.v_stream = nn.Sequential(nn.Linear(in_features=d_cat, out_features=d_cat//2),
+                                    nn.Linear(in_features=d_cat//2, out_features=d_cat//4),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=dropout_prob),
+                                    nn.Linear(in_features=d_cat//4, out_features=d_cat//2),
+                                    nn.Linear(in_features=d_cat//2, out_features=d_cat))
+        self.fusion_stream = nn.Sequential(nn.Linear(in_features=2*d_cat, out_features=d_cat),
+                                            nn.ReLU(),
+                                            nn.Dropout(p=dropout_prob),
+                                            nn.Linear(in_features=d_cat, out_features=d_model))
+                                            
+    def forward(self, decoder_batch, history_cntx, visual_cntx):
+        assert decoder_batch.dim() == 3, 'Expected 3 dimensions, got {}'.format(decoder_batch.dim())
+        assert history_cntx.shape[-1] == self.d_context, 'History dimension {} does not match d_context of {}'.format(history_cntx.shape[-1], self.d_context)
+        assert history_cntx.shape[-1] == visual_cntx.shape[-1], 'History and visual context sizes do not match'
+
+        h_in = torch.cat((decoder_batch, history_cntx.unsqueeze(1).expand(-1, decoder_batch.shape[1], -1)), dim=-1)
+        v_in = torch.cat((decoder_batch, visual_cntx.unsqueeze(1).expand(-1, decoder_batch.shape[1], -1)), dim=-1)
+        h_out = self.v_stream(h_in)
+        v_out = self.h_stream(v_in)
+        fuse_in = torch.cat((h_out, v_out), dim=-1)
+        fuse_out = self.fusion_stream(fuse_in)
+        return fuse_out
 
 
 
@@ -206,8 +250,8 @@ class MultiHeadSelfAttention(nn.Module):
         self.attn_heads = nn.ModuleList([SelfAttention(d_model, d_k, d_v, dropout_prob) for _ in range(n_heads)])
 
 
-    def forward(self, input_batch, input_mask):
-        outs = torch.stack([attn_head(input_batch, input_mask) for attn_head in self.attn_heads])
+    def forward(self, input_batch, attn_mask):
+        return torch.cat([attn_head(input_batch, attn_mask) for attn_head in self.attn_heads], dim=-1)
 
 
     def __str__(self):
@@ -221,6 +265,10 @@ class MultiHeadEncoderAttention(nn.Module):
 
         self.n_heads = n_heads
         self.attn_heads = nn.ModuleList([EncoderAttention(d_model, d_enc, d_k, d_v, dropout_prob) for _ in range(n_heads)])
+
+
+    def forward(self, decoder_batch, encoder_out, attn_mask):
+        return torch.cat([attn_head(decoder_batch, encoder_out, attn_mask) for attn_head in self.attn_heads], dim=-1)
 
 
     def __str__(self):
@@ -240,14 +288,14 @@ class SelfAttention(nn.Module):
         self.dropout = nn.Dropout(p=dropout_prob)
 
 
-    def forward(self, input_batch, input_mask):
+    def forward(self, input_batch, attn_mask):
 
         query = self.Q(input_batch)
         key = self.K(input_batch)
         value = self.V(input_batch)
 
         attn_logits = torch.matmul(query, torch.transpose(key, -2, -1))/ math.sqrt(self.d_k)
-        masked_attn_logits = attn_logits.masked_fill(input_mask==0, -np.inf)
+        masked_attn_logits = attn_logits.masked_fill(attn_mask==0, -np.inf)
         attn_scores = F.softmax(masked_attn_logits, -1)
         #pad attentions are row filled with 0's. The softmax will then output NaN for these row.
         # the following line just replace NaN values with 0's
@@ -283,7 +331,7 @@ class EncoderAttention(nn.Module):
         attn_logits = torch.matmul(query, torch.transpose(key, -2, -1))/ math.sqrt(self.d_k)
         masked_attn_logits = attn_logits.masked_fill(attn_mask==0, -np.inf)
         attn_scores = F.softmax(masked_attn_logits, -1)
-        #pad attentions are row filled with 0's. The softmax will then output NaN for these row.
+        #pad attentions are row filled with 0's. The softmax will then output NaN for these rows.
         # the following line just replace NaN values with 0's
         attn_scores[attn_scores != attn_scores] = 0
         attn_scores = self.dropout(attn_scores)

mm_response_generation/models/mmstateful.py
@@ -42,7 +42,7 @@ class MMStatefulLSTM(nn.Module):
         self.start_id = word2id[start_token]
         self.end_id = word2id[end_token]
         n_encoders, n_enc_heads = 1, 4
-        n_decoders, n_dec_heads = 1, 4
+        n_decoders, n_dec_heads = 4, 4
         
         #self.item_embeddings_layer = ItemEmbeddingNetwork(item_embeddings_path)
         self.word_embeddings_layer = WordEmbeddingNetwork(word_embeddings_path=word_embeddings_path, 
@@ -79,6 +79,7 @@ class MMStatefulLSTM(nn.Module):
             #for h heads: d_k == d_v == emb_dim/h
             self.decoder = Decoder(d_model=self.emb_dim,
                                     d_enc=2*self._HIDDEN_SIZE,
+                                    d_context=self._HIDDEN_SIZE,
                                     d_k=self.emb_dim//n_dec_heads,
                                     d_v=self.emb_dim//n_dec_heads,
                                     d_f=self.emb_dim//2,
@@ -163,21 +164,24 @@ class MMStatefulLSTM(nn.Module):
             true_responses = candidates_pool if self.training else candidates_pool[:, 0]
             #responses_emb = self.word_embeddings_layer(true_responses)
             #true_responses shape BxSEQ_LEN ; responses_emb shape BxSEQ_LENxHIDDEN_SIZE
-            context = torch.cat((h_t_tilde, v_t_tilde), dim=-1)
-            self.decoder(input_batch=true_responses,
-                        encoder_out=u_t_all,
-                        context=context,
-                        enc_mask=utterances_mask,
-                        input_mask=pools_padding_mask)
+            #context = torch.cat((h_t_tilde, v_t_tilde), dim=-1)
+            vocab_logits = self.decoder(input_batch=true_responses,
+                                        encoder_out=u_t_all,
+                                        history_context=h_t_tilde,
+                                        visual_context=v_t_tilde,
+                                        enc_mask=utterances_mask,
+                                        input_mask=pools_padding_mask)
+            return vocab_logits
+        else:
+            raise Exception('Not implemented')
+            """
             #encode candidates
             candidates_emb = self.word_embeddings_layer(candidates_pool)
             encoded_candidates = torch.stack([self.sentence_encoder(candidates)[0] for candidates in candidates_emb])
             #try the fusion with a fnn also
             turns_repr = v_t_tilde + h_t_tilde
             out = self.out_layer(turns_repr, encoded_candidates)
-        else:
-            raise Exception('Not implemented')
-        return out
+            """
 
 
     def encode_v_context(self, focus_images):
@@ -244,16 +248,16 @@ class MMStatefulLSTM(nn.Module):
 
         #pad the response candidates
         # if training take only the true response
-        if self.training:
-            responses_pool = [pool_sample[0] for pool_sample in responses_pool]
-            batch_lens = torch.tensor(list(map(len, responses_pool)), dtype=torch.long)
-            pools_tensor = torch.zeros((len(responses_pool), batch_lens.max()+2), dtype=torch.long)
-            pools_padding_mask = torch.zeros((len(responses_pool), batch_lens.max()+2), dtype=torch.long)
-            pools_tensor[:, 0] = self.start_id
-            for batch_idx, (seq, seqlen) in enumerate(zip(responses_pool, batch_lens)):
-                pools_tensor[batch_idx, 1:seqlen+1] = seq.clone().detach()
-                pools_tensor[batch_idx, seqlen+1] = self.end_id
-                pools_padding_mask[batch_idx, :seqlen+2] = 1
+        responses_pool = [pool_sample[0] for pool_sample in responses_pool]
+        batch_lens = torch.tensor(list(map(len, responses_pool)), dtype=torch.long)
+        pools_tensor = torch.zeros((len(responses_pool), batch_lens.max()+2), dtype=torch.long)
+        pools_padding_mask = torch.zeros((len(responses_pool), batch_lens.max()+2), dtype=torch.long)
+        pools_tensor[:, 0] = self.start_id
+        for batch_idx, (seq, seqlen) in enumerate(zip(responses_pool, batch_lens)):
+            pools_tensor[batch_idx, 1:seqlen+1] = seq.clone().detach()
+            pools_tensor[batch_idx, seqlen+1] = self.end_id
+            pools_padding_mask[batch_idx, :seqlen+2] = 1
+        """
         else:
             batch_lens = torch.tensor([list(map(len, pool_sample)) for pool_sample in responses_pool], dtype=torch.long)
             pools_tensor = torch.zeros((len(responses_pool), len(responses_pool[0]), batch_lens.max()+2), dtype=torch.long)
@@ -264,6 +268,7 @@ class MMStatefulLSTM(nn.Module):
                     pools_tensor[batch_idx, pool_idx, 1:seqlen+1] = seq.clone().detach()
                     pools_tensor[batch_idx, pool_idx, seqlen+1] = self.end_id
                     pools_padding_mask[batch_idx, pool_idx, :seqlen+2] = 1
+        """
 
         #pad focus items
         padded_focus = []

mm_response_generation/train.py
@@ -18,7 +18,7 @@ from models import BlindStatelessLSTM, MMStatefulLSTM
 from utilities import Logger, plotting_loss, DataParallelV2
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="0"  # specify which GPU(s) to be used
+os.environ["CUDA_VISIBLE_DEVICES"]="5"  # specify which GPU(s) to be used
 
 
 def instantiate_model(args, word2id, device):
@@ -73,19 +73,29 @@ def move_batch_to_device(batch, device):
     batch['seq_lengths'] = batch['seq_lengths'].to(device)
 
 
-def forward_step(model, batch, candidates_pool, pools_padding_mask, response_criterion, device):
-    #todo cross entropy but for regression (not a classification problem, maximize the first score using softmax will minime also the others)
+def forward_step(model, batch, targets, targets_padding_mask, response_criterion, device):
     move_batch_to_device(batch, device)
-    candidates_pool = candidates_pool.to(device)
-    pools_padding_mask = pools_padding_mask.to(device)
+    targets = targets.to(device)
+    targets_padding_mask = targets_padding_mask.to(device)
 
-    matching_logits = model(**batch,
-                            candidates_pool=candidates_pool,
-                            pools_padding_mask=pools_padding_mask)
+    vocab_logits = model(**batch,
+                        candidates_pool=targets,
+                        pools_padding_mask=targets_padding_mask)
+    #todo problem here: CE expect each batch sample to belong to one class, here instead I have a sequence of results for each sample (3D tensor)
+    #targets are shifted right by one
+    shifted_targets = torch.cat((targets[:, 1:], torch.zeros((targets.shape[0], 1), dtype=torch.long).to(device)), dim=-1)
+    pdb.set_trace()
+    response_loss = response_criterion(vocab_logits.view(vocab_logits.shape[0]*vocab_logits.shape[1], -1), 
+                                        shifted_targets.view(vocab_logits.shape[0]*vocab_logits.shape[1]))
+    #todo better to create input and target in the _collate_fn
+    #todo masking here no more possible (loss is a single value at this point)
+    response_loss = torch.masked_select(response_loss, targets_padding_mask)
 
+    """
     # the true response is always the first in the list of candidates
     matching_targets = torch.ones(batch['utterances'].shape[0], dtype=torch.long).to(device)
     response_loss = response_criterion(matching_logits, matching_targets)
+    """
 
     return response_loss
 
@@ -156,13 +166,13 @@ def train(train_dataset, dev_dataset, args, device):
         model.train()
         curr_epoch_losses = []
         # sorted_dial_ids, sorted_dial_turns, batch_dict, sorted_responses, sorted_distractors
-        for curr_step, (dial_ids, turns, batch, candidates_pool, pools_padding_mask) in enumerate(trainloader):
+        for curr_step, (dial_ids, turns, batch, targets, targets_padding_mask) in enumerate(trainloader):
             #step_start = time.time()
             #print(curr_step)
             response_loss = forward_step(model, 
                                         batch=batch,
-                                        candidates_pool=candidates_pool,
-                                        pools_padding_mask=pools_padding_mask,
+                                        targets=targets,
+                                        targets_padding_mask=targets_padding_mask,
                                         response_criterion=response_criterion,
                                         device=device)
             #backward
@@ -181,11 +191,11 @@ def train(train_dataset, dev_dataset, args, device):
         model.eval()
         curr_epoch_losses = []
         with torch.no_grad(): 
-            for curr_step, (dial_ids, turns, batch, candidates_pool, pools_padding_mask) in enumerate(devloader):
+            for curr_step, (dial_ids, turns, batch, targets, targets_padding_mas) in enumerate(devloader):
                 response_loss = forward_step(model, 
                                             batch=batch,
-                                            candidates_pool=candidates_pool,
-                                            pools_padding_mask=pools_padding_mask,
+                                            targets=targets,
+                                            targets_padding_mask=targets_padding_mask,
                                             response_criterion=response_criterion,
                                             device=device)
                 curr_epoch_losses.append(response_loss.item())
@@ -220,7 +230,6 @@ def train(train_dataset, dev_dataset, args, device):
     if not args.checkpoints:
         checkpoint_dir = None
     plotting(epochs=args.epochs, losses_trend=losses_trend, checkpoint_dir=checkpoint_dir)
-
 
 
 